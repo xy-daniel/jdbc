7f8af87a5c844d992ec82edd1bb9d13b7d5c43c5
86178 <1207350458@qq.com>
2020-03-10 14:27:48 +0800

    '项目初始化'

---
+++ src/test/java/org/javaboy/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
